551bc84612291eb2ea484a01c8cab03b0069122a
plank release <release@plankhq.io>
2026-08-25 16:42:39 +0530

    Update plank to 0.0.3

--- Formula/plank.rb
+++ Formula/plank.rb
@@ -1,25 +1,25 @@
 class Plank < Formula
   desc "Plank - Agent first CI/CD and Workflow Orchestration"
   homepage "https://plankhq.io"
-  version "0.0.2"
+  version "0.0.3"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/plankhq/homebrew-tap/releases/download/v0.0.2/plank-local-0.0.2-aarch64-apple-darwin.tar.gz"
-      sha256 "5c663f1208933f7e1b3512393baf8e4a9e5ee7394694b6f1234dd492545d41b8"
+      url "https://github.com/plankhq/homebrew-tap/releases/download/v0.0.3/plank-local-0.0.3-aarch64-apple-darwin.tar.gz"
+      sha256 "7056415ea23fd052c78fc9862ebde17ea45e04c2ec9324eee85a35da0fc6f007"
     else
-      url "https://github.com/plankhq/homebrew-tap/releases/download/v0.0.2/plank-local-0.0.2-x86_64-apple-darwin.tar.gz"
-      sha256 "144c920ef865e5e1556d0f80b84047befd7b25f66bdb3d1593bab9329ff8ac04"
+      url "https://github.com/plankhq/homebrew-tap/releases/download/v0.0.3/plank-local-0.0.3-x86_64-apple-darwin.tar.gz"
+      sha256 "c2458d073a28c69157d163c150acd20a6307a14e380347fb5e6fd5362968d170"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/plankhq/homebrew-tap/releases/download/v0.0.2/plank-local-0.0.2-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "9c80e5879bdb09b52a83089ce2810b4f12e5eb78eafc6d34d4a593deb995b75d"
+      url "https://github.com/plankhq/homebrew-tap/releases/download/v0.0.3/plank-local-0.0.3-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "867ec5cd0d81651e714ada9f064b9511cf1cf0878de639bd672f3fbcb24a9e5e"
     else
-      url "https://github.com/plankhq/homebrew-tap/releases/download/v0.0.2/plank-local-0.0.2-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "432c140d6e48a9b357359a48a85c963e415898bee236109f9f875a98193a29a2"
+      url "https://github.com/plankhq/homebrew-tap/releases/download/v0.0.3/plank-local-0.0.3-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "5866a53854b157fc6c57e274bdd114acda4930b1ddca0bf389bef97cbcbce153"
     end
   end
 
