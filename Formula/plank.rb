a5dada2817a27bbb415af82845d9329da17273b5
plank release <release@plankhq.io>
2026-09-01 14:42:03 +0530

    Update plank to 0.0.41

--- Formula/plank.rb
+++ Formula/plank.rb
@@ -1,25 +1,25 @@
 class Plank < Formula
   desc "Plank - Agent first CI/CD and Workflow Orchestration"
   homepage "https://plankhq.io"
-  version "0.0.4"
+  version "0.0.41"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/plankhq/homebrew-tap/releases/download/v0.0.4/plank-local-0.0.4-aarch64-apple-darwin.tar.gz"
-      sha256 "8c0b14c5ed964f2e7c2a4df98b094904fe629f4f3c34eff7850334cd0852706d"
+      url "https://github.com/plankhq/homebrew-tap/releases/download/v0.0.41/plank-local-0.0.41-aarch64-apple-darwin.tar.gz"
+      sha256 "a57028d81dec2976509105466313150453d0a39f89c8cc8bf64542525a96d1f7"
     else
-      url "https://github.com/plankhq/homebrew-tap/releases/download/v0.0.4/plank-local-0.0.4-x86_64-apple-darwin.tar.gz"
-      sha256 "3438fed3226a3fe8783419c6551c9c9b8667f6feaddea7ab53e00cf0bc02b086"
+      url "https://github.com/plankhq/homebrew-tap/releases/download/v0.0.41/plank-local-0.0.41-x86_64-apple-darwin.tar.gz"
+      sha256 "f57fde43ea7c5fc215e061d540202424ed1c25d6dd46e0fca916786f128a5ac4"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/plankhq/homebrew-tap/releases/download/v0.0.4/plank-local-0.0.4-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "76cff3393891d5b37f16c5d73973e1fe4e6b08d8fbc28ed7a1b8c1f668c62272"
+      url "https://github.com/plankhq/homebrew-tap/releases/download/v0.0.41/plank-local-0.0.41-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "a7e9d029b50bb635542532773f993ab5d875c863122b1cfa1d34eec9dedcf384"
     else
-      url "https://github.com/plankhq/homebrew-tap/releases/download/v0.0.4/plank-local-0.0.4-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "55b27d74eae95c430221c1ac5bd412d586a85e77da8afab33003898a05f943bb"
+      url "https://github.com/plankhq/homebrew-tap/releases/download/v0.0.41/plank-local-0.0.41-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "bac58c365ec665c01f974b24f88921a863ac0de70e73821e012bc448239b08b0"
     end
   end
 
